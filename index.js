e87ec85f679ac2f36176279d64a602ded13060b0
samagatova456 <samagatovanazik0@gmail.com>
2023-01-18 12:09:56 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+let myName = "Nazik";
+
+console.log(myName);
+console.log(myName.length);
+console.log(myName.toLowerCase());
+console.log(myName.toUpperCase());
+
+let pi = 3.41593;
+
+console.log(pi);
+console.log(pi.toFixed(2));
+console.log(pi.toFixed(0));
+
+let example1 = 2 + 2;
+let example2 = 2 - 2;
+let example3 = 2 * 2; // 12
+let example4 = 2 / 2; // 12
+
+let example5 = 2 ** 4; //степень (квадрат)
+let example6 = 81 ** (1/3); // кубический корень
+
+let example7 = 5 / 2; // остаток
+
+let example8 = "hello" + "World" + '!';
+
+let example9 = 4 + 10 + 12;
+let example10 = 1 + 5 * 3;
+let example11 = 15 * (1 - 1);
+
+let example12 = 5 + '3';
+let example13 = '5' + 3;
+let example14 = '5' * 3; // 15
+let example15 = 'a' * 3; // NaN
+
+let example16 = -15;
+let example17 = +'15';
+console.log(example15); 
